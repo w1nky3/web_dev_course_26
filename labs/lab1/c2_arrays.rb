646a84e78798803c1fbf0a1f4876f6e7456f5c8f
Экизян Владимир <9885313888va@gmail.com>
2026-02-10 14:37:11 +0000

    Array Operations

--- labs/lab1/c2_arrays.rb
+++ labs/lab1/c2_arrays.rb
@@ -10,7 +10,7 @@
 # Expected: first_and_last([1, 2, 3, 4]) => [1, 4]
 def first_and_last(arr)
   # TODO: Use array indexing with [0] and [-1]
-  nil
+  [arr[0], arr[-1]]
 end
 
 # Exercise 2: Array methods
@@ -18,7 +18,8 @@ end
 # Expected: add_element([1, 2, 3], 4) => [1, 2, 3, 4]
 def add_element(arr, element)
   # TODO: Use .push or << operator (modify and return the array)
-  nil
+  arr.push(element)
+  return arr
 end
 
 # Exercise 3: Array iteration
@@ -26,7 +27,7 @@ end
 # Expected: double_numbers([1, 2, 3]) => [2, 4, 6]
 def double_numbers(arr)
   # TODO: Use .map method
-  nil
+  return arr.map{|i| i * 2}
 end
 
 # Exercise 4: Array filtering
@@ -34,7 +35,7 @@ end
 # Expected: even_numbers([1, 2, 3, 4, 5, 6]) => [2, 4, 6]
 def even_numbers(arr)
   # TODO: Use .select or .filter method
-  nil
+  return arr.select{|x| x % 2 == 0}
 end
 
 # Exercise 5: Array reduction
@@ -42,7 +43,7 @@ end
 # Expected: sum_array([1, 2, 3, 4]) => 10
 def sum_array(arr)
   # TODO: Use .sum or .reduce method
-  nil
+  return arr.sum
 end
 
 # Exercise 6: Array operations
@@ -50,7 +51,7 @@ end
 # Expected: contains?([:a, :b, :c], :b) => true
 def contains?(arr, element)
   # TODO: Use .include? method
-  nil
+  return arr.include?(element)
 end
 
 # Exercise 7: Array sorting
@@ -58,7 +59,8 @@ end
 # Expected: sort_desc([3, 1, 4, 1, 5]) => [5, 4, 3, 1, 1]
 def sort_desc(arr)
   # TODO: Use .sort with .reverse or .sort_by
-  nil
+  arr = arr.sort()
+  return arr.reverse()
 end
 
 # Exercise 8: Array range
@@ -66,7 +68,7 @@ end
 # Expected: number_range(1, 5) => [1, 2, 3, 4, 5]
 def number_range(start, finish)
   # TODO: Use (start..finish).to_a
-  nil
+  return (start..finish).to_a()
 end
 
 # ============================================
